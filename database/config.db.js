9b5ec1b7266db544e5f1eef93df072b7b77ff220
Camilo Galvis <cglv11@gmail.com>
2024-08-12 16:13:38 -0500

    feat: adding singleton con db connection

--- database/config.db.js
+++ database/config.db.js
@@ -1,15 +1,29 @@
 const { PrismaClient } = require('@prisma/client');
 
-const prisma = new PrismaClient();
-
-const connectDb = async () => {
-    try {
-        await prisma.$connect();
-        console.log('Database connected successfully.');
-    } catch (error) {
-        console.error('Error connecting to the database:', error);
-        process.exit(1);
+class Database {
+  constructor() {
+    if (!Database.instance) {
+      this.prisma = new PrismaClient();
+      Database.instance = this;
     }
-};
 
-module.exports = { prisma, connectDb };
+    return Database.instance;
+  }
+
+  async connectDb() {
+    try {
+      await this.prisma.$connect();
+      console.log('Database connected successfully.');
+    } catch (error) {
+      console.error('Error connecting to the database:', error);
+      process.exit(1);
+    }
+  }
+}
+
+const dbInstance = new Database();
+
+module.exports = {
+  prisma: dbInstance.prisma,
+  connectDb: dbInstance.connectDb.bind(dbInstance)
+};
